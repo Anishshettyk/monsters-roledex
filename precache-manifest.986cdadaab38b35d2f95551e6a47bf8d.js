c0d5affc93bd50fb787a7712a89f3e96856fb343
Anish shetty k <47324253+Anishshettyk@users.noreply.github.com>
2020-09-17 21:21:09 +0530

    Updates

---
+++ precache-manifest.986cdadaab38b35d2f95551e6a47bf8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bde2a21f6b978d9cf8225589c2a2b05c",
+    "url": "/monsters-roledex/index.html"
+  },
+  {
+    "revision": "bca37b3ea3bff4897cb6",
+    "url": "/monsters-roledex/static/css/main.29ad3700.chunk.css"
+  },
+  {
+    "revision": "18e3087de538d91ee632",
+    "url": "/monsters-roledex/static/js/2.2a71acd0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-roledex/static/js/2.2a71acd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bca37b3ea3bff4897cb6",
+    "url": "/monsters-roledex/static/js/main.0e14f5a0.chunk.js"
+  },
+  {
+    "revision": "190f18fc45d28b0800c4",
+    "url": "/monsters-roledex/static/js/runtime-main.4be1a078.js"
+  }
+]);
